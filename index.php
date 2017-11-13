28f8529306a2e0619f9eb9f8859ec792ac7870f2
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-13 11:10:49 +0100

    Add html and bootstrap cdn

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+    <title>Gestion des comptes bancaires</title>
+</head>
+<body>
+    <!-- Latest compiled and minified JS -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery.js"></script>
+</body>
+</html>
