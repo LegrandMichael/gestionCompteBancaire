23c85bc84664350de7f43b1e295388fb71467cda
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-14 14:47:41 +0100

    Ajout des méthodes public getNom, getId et getPrenom

--- Class/Personne.php
+++ Class/Personne.php
@@ -1,11 +1,33 @@
 <?php
 
-require 'Autoloader.php'; 
-Autoloader::register();
-
 class Personne {
     
-    
-}
+    protected $id;
+    protected $nom;
+    protected $prenom;
 
+    protected static $prochainId = 1;
+
+    public function __construct($prenom,$nom){
+        $this->id=self::$prochainId++;
+        $this->nom=$nom;
+        $this->prenom=$prenom;
+    }
+
+    public function getNom(){
+        echo $this->nom;
+    }
+
+    public function getPrenom(){
+        return $this->prenom;
+    }
+
+    public function getId(){
+        echo $this->id;
+    }
+
+    public function __toString(){
+        return "Personne Id: " . $this->id. "Prénom:".$this->prenom."Nom ". $this->nom;
+    }
+}
 ?>
