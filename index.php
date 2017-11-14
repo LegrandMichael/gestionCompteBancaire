509a226ed301cc87c0f2550f17e13390eefd04e2
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-14 16:18:18 +0100

    Ajout modal pour la création de client et de compte

--- index.php
+++ index.php
@@ -20,11 +20,51 @@ Autoloader::register();
 <body>
     <div class="page-header">
         <h1 class="text-center">Gestion des comptes bancaires<br><small>Simplon Carcassonne</small></h1>
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary">Créer un compte</button>
+        <div class="text-center">            
+            <a class="btn btn-primary" data-toggle="modal" href='#creerCompte'>Créer un compte</a>
+            <div class="modal fade" id="creerCompte">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="submit" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Création de compte</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form action="" method="POST" role="form">
+                                <legend>Form title</legend>
+                                <div class="form-group">
+                                    <label for="">label</label>
+                                    <input type="text" class="form-control" id="" placeholder="Input field">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                            <button type="button" class="btn btn-primary">Sauvegarder</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="btn btn-primary" data-toggle="modal" href='#creerClient'>Créer un client</a>
+            <div class="modal fade" id="creerClient">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Modal title</h4>
+                        </div>
+                        <div class="modal-body">
+                            
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
-        
-            <button type="submit" class="btn btn-primary">Créer un client</button>
         </div>
     </div>
     <div class="container">
@@ -43,7 +83,8 @@ Autoloader::register();
                             <?php 
                             $perso1 = new Personne("Jean", "Dujardin");
                             $perso2 = new Personne("Michaël", "Legrand");
-                            $persoTotal = [$perso1,$perso2];
+                            $perso3 = new Personne('Yves', 'Montand');
+                            $persoTotal = [$perso1,$perso2,$perso3];
                             //var_dump($persoTotal);
                                 foreach ( $persoTotal as $key => $value) {
                                     # code...
@@ -63,7 +104,7 @@ Autoloader::register();
         </div>
     </div>
     <!-- Latest compiled and minified JS -->
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
