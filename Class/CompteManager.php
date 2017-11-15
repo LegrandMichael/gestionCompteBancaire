8dc9462f6772144b2b2600b3e1e24cc60ec7cfd3
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-15 16:51:45 +0100

    Classe CompteManager

---
+++ Class/CompteManager.php
@@ -0,0 +1,22 @@
+<?php 
+
+class CompteManager {
+
+    private $compte = [Compte];
+    private $lastNumCompte;
+    private $depassementAuto;
+    private $depotInitial;
+
+    public function __construct(){
+
+    }
+
+    public function ouvrirCompte(){
+
+    }
+
+    public function cloturerCompte(){
+        
+    }
+
+}
