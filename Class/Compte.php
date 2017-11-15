876b0eee405ac207b4a69aded311ee01a7748b45
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-15 16:48:55 +0100

    Classe compte

---
+++ Class/Compte.php
@@ -0,0 +1,28 @@
+<?php
+
+class Compte {
+
+    private $numero;
+    private $solde;
+    private $operations = [Operation];
+    private $client = Cient;
+    private $conseiller = Conseiller;
+    private $depassementAutorise;
+
+    public function __construct() {
+
+    }
+
+    public function credit(){
+
+    }
+
+    public function debit(){
+
+    }
+
+    public function bloquer(){
+        
+    }
+
+}
